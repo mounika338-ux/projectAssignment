a534d09c50de325971ccd3b26b8a73e3d5e25bb0
mounika338-ux <paravasthu338@gmail.com>
2025-10-15 20:25:37 +0530

    Db Config

--- db/configDB.js
+++ db/configDB.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 export const connectDB=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("mongodb connected successfully");
+        console.log("Database Connected Successfully");
     }
     catch(err){
         console.log(" db not connected",err.message);
